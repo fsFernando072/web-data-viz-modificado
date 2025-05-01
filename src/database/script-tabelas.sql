a242c73c195b182a24b3fae192234420e5d84863
fsFernando072 <fernando.fsilva@sptech.school>
2025-05-01 12:05:20 -0300

    Adicionando coluna de CPF no BD

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -60,3 +60,5 @@ insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B
 insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
 insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
 insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
+
+ALTER TABLE usuario ADD COLUMN cpf CHAR(11);
